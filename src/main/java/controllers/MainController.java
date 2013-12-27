427aef02149c1d073876c90ea4743f18ee20565c
Anton Krupnov <oursstartup@gmail.com>
2013-12-27 22:11:07 +0200

    Init

---
+++ src/main/java/controllers/MainController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/home")
+    public String home() {
+        return "WEB-INF/jsp/tourList.jsp";
+    }
+}
